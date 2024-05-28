2b7fcc3b5a05caa062e5de0d5d463eb0839b6d68
Manpreet Singh <manpreetsingh5772000@gmail.com>
2024-05-28 22:49:23 +0530

    remove session

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,21 +2,13 @@ import express, {Express} from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import rateLimiter from './middleware/rateLimit';
-import session from 'express-session';
 import { firebaseAuth } from './middleware/firebase';
 import { scriptRouter } from './api/script/scriptRouter';
 import { userRouter } from './api/user/userRouter';
 
 const app:Express = express();
 
-app.use(
-    session({
-      secret: 'your_secret_key', // Replace with a strong secret
-      resave: false,
-      saveUninitialized: false,
-    })
-  );
-  
+
 app.set('trust proxy', true); // Trust all proxies
 
 app.use(cors()); 
